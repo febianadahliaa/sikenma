abd35061b3b89ca04074f65c730bbbe8cf8e40ce
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-09-23 15:35:46 +0800

    fix auth controller

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,15 +6,111 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        // $this->load->model('auth_model');
         $this->load->library('form_validation');
     } //akan selalu dirun di semua method
 
     public function index()
     {
-        $data['title'] = 'Login SIKENMA';
-        $this->load->view('partials/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('partials/auth_footer');
+        $email = $this->session->userdata('email');
+        $role =  $this->session->userdata('roleId');
+
+        if ($email == NULL) {
+            $this->login();
+        } else {
+            if ($role == 1) {
+                redirect('admin');
+            } elseif ($role == 2) {
+                redirect('user');
+            }
+        }
+    }
+
+    public function login()
+    {
+        $config = [
+            [
+                'field' => 'email',
+                'label' => 'Email',
+                'rules' => 'trim|required|valid_email',
+                'errors' => [
+                    'required' => 'Email harus diisi!',
+                    'valid_email' => 'Email tidak valid!'
+                ]
+            ],
+            [
+                'field' => 'password',
+                'label' => 'Password',
+                'rules' => 'trim|required',
+                'errors' => [
+                    'required' => 'Password harus diisi!'
+                ]
+            ]
+        ];
+        $this->form_validation->set_rules($config);
+
+        if ($this->form_validation->run()) {
+            $email = $this->input->post('email');
+            $password = $this->input->post('password');
+            // $user = $this->auth_model->getUserByEmail($email);
+
+            // KENAPA GA BISA PAKE MODEL???
+            $query = $this->db->select('user.*, user_position.position, district.district')
+                ->from('user')
+                ->where('email', $email)
+                ->join('user_position', 'user.position_id = user_position.id', 'Left')
+                ->join('district', 'user.district_id = district.id', 'Left')
+                ->get();
+            $user = $query->row_array();
+
+
+            if ($user) {
+                if (password_verify($password, $user['password'])) {
+                    //Jika berhasil login, maka data yang disimpan dalam session
+                    $data = [
+                        'email' => $user['email'],
+                        'roleId' => $user['role_id'],
+                        'nip' => $user['nip'],
+                        'loc' => $user['district'],
+                        'name' => $user['name'],
+                        'position' => $user['position'],
+                        'gender' => $user['gender']
+                    ];
+                    $this->session->set_userdata($data);
+
+                    if ($user['role_id'] == 1) { //admin
+                        redirect('admin');
+                    } elseif ($user['role_id'] == 2) { //member
+                        redirect('user');
+                    }
+                } else {
+                    $this->session->set_flashdata('error', 'Password salah!');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('error', 'Email tidak terdaftar!');
+                redirect('auth');
+            }
+        } else {
+            $data['title'] = 'Login SIKENMA';
+            $this->load->view('partials/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('partials/auth_footer');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('roleId');
+        $this->session->unset_userdata('nip');
+        $this->session->unset_userdata('loc');
+        $this->session->unset_userdata('name');
+        $this->session->unset_userdata('position');
+        $this->session->unset_userdata('gender');
+
+        $this->session->set_flashdata('message', 'Anda sudah keluar!');
+        redirect('auth');
     }
 
     public function register()
@@ -68,11 +164,13 @@ class Auth extends CI_Controller
             $data = [
                 'nama' => htmlspecialchars($this->input->post('name', true)),
                 'email' => htmlspecialchars($this->input->post('email', true)),
-                'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
-                'id_role' => 2,
+                'image' => 'default.jpg',
+                'gender' => '',
+                'position_id' => 2,
+                'role_id' => 2,
                 'phone' => '',
-                'id_kecamatan' => 7407050
+                'district_id' => 7407050
             ];
 
             $this->db->insert('user', $data);
