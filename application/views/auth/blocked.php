3520d182bc8ea4df8122606b750693b97778e806
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-09-30 14:35:55 +0800

    initial commit

---
+++ application/views/auth/blocked.php
@@ -0,0 +1,18 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- 404 Error Text -->
+    <div class="text-center">
+        <div class="error mx-auto mb-4" data-text="403">403</div>
+        <!-- <p class="lead text-gray-800 mb-5">Anda tidak diizinkan untuk mengakses halaman ini.</p> -->
+        <p class="lead text-gray-800 mb-1">このページへのアクセスは禁止されています！</p>
+        <p class="lead text-gray-800 mb-5">Access to this page is prohibited!</p>
+        <!-- <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p> -->
+        <a href="<?= base_url('beranda/index'); ?>">&larr; Kembali ke Halaman Utama</a>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
