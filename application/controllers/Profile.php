979c929675a1504c3196b526027fea3d8bce4488
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-09-30 14:33:01 +0800

    initial commit

---
+++ application/controllers/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Profil Saya';
+        $data['subMenuName'] = '';
+
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $query = $this->db->select('user.*, user_position.position, district.district')
+            ->from('user')
+            ->where('email', $this->session->userdata('email'))
+            ->join('user_position', 'user.position_id = user_position.id', 'Left')
+            ->join('district', 'user.district_id = district.id', 'Left')
+            ->get();
+        $data['user'] = $query->row_array();
+
+        $this->load->view('partials/header', $data);
+        $this->load->view('partials/sidebar', $data);
+        $this->load->view('partials/topbar', $data);
+        $this->load->view('profile', $data);
+        $this->load->view('partials/footer');
+    }
+}
---
+++ application/views/profile.php
@@ -0,0 +1,40 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page heading -->
+    <div class="row">
+        <div class="col-lg-6">
+            <h3 class="mb-4 text-gray-800"><strong><?= $title ?></strong></h3>
+        </div>
+    </div>
+
+    <!-- Page Content -->
+    <div class="card mb-3" style="max-width: 720px;">
+        <div class="row no-gutters">
+            <div class="col-md-4" style="margin: 1em;">
+                <img class="rounded mx-auto d-block" src="<?php if ($user['gender'] == 'female') echo base_url('assets/img/undraw_profile_1.svg');
+                                                            else echo base_url('assets/img/undraw_profile_2.svg'); ?>">
+            </div>
+            <div class="col-md-7">
+                <div class="card-body">
+                    <div class="text-center">
+                        <p class="card-title h3"><?= $user['name']; ?></p>
+                    </div>
+                    <p class="card-text h5"><i class="far fa-fw fa-id-badge"></i> <?= $user['nip']; ?></p>
+                    <p class="card-text h5"><i class="far fa-fw fa-envelope"></i> <?= $user['email']; ?></p>
+                    <p class="card-text h5"><i class="fas fa-fw fa-rocket"></i> <?= $user['position']; ?></p>
+                    <p class="card-text h5"><i class="fas fa-fw fa-phone-square"></i> <?= $user['phone']; ?></p>
+                    <p class="card-text h5"><i class="fas fa-fw fa-map-marker-alt"></i> <?= $user['district']; ?></p>
+
+                    <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
