aa1e82488f53942462dc645435e9b13e459474f1
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-10-03 09:39:45 +0800

    fix mitra data

--- application/controllers/database_mitra/Mitra_data.php
+++ application/controllers/database_mitra/Mitra_data.php
@@ -14,6 +14,9 @@ class Mitra_data extends CI_Controller
         $data['title'] = 'Data Mitra';
         $data['subMenuName'] = 'Data Mitra';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['mitra'] = $this->db->get('mitra')->result_array();
+        $data['activity'] = $this->db->get('activity')->result_array();
+        $data['reviewer'] = $this->db->get('user')->result_array();
 
         $query = $this->db->join('mitra', 'mitra_track_record.mitra_id = mitra.id')
             ->join('village', 'mitra.village_id = village.id')
@@ -28,5 +31,39 @@ class Mitra_data extends CI_Controller
         $this->load->view('partials/topbar', $data);
         $this->load->view('database_mitra/mitra-data', $data);
         $this->load->view('partials/footer');
+    }
+
+    public function addRecord()
+    {
+        $this->form_validation->set_rules('mitra_id', 'Name', 'required');
+        $this->form_validation->set_rules('activity_id', 'Activity', 'required');
+        $this->form_validation->set_rules('year', 'Year', 'required|trim|min_length[4]|max_length[4]');
+        $this->form_validation->set_rules('skor_geo', 'GEO', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('skor_it', 'IT', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('skor_probing', 'PROB', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('skor_quality', 'QTY', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('skor_abc', 'ABC', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('skor_time', 'Time', 'required|trim|min_length[1]|max_length[3]');
+        $this->form_validation->set_rules('user_id', 'Reviewer', 'required');
+
+        if ($this->form_validation->run()) {
+            $data = [
+                'mitra_id' => $this->input->post('mitra_id'),
+                'activity_id' => $this->input->post('activity_id'),
+                'year' => $this->input->post('year'),
+                'skor_geo' => $this->input->post('skor_geo'),
+                'skor_it' => $this->input->post('skor_it'),
+                'skor_prob' => $this->input->post('skor_probing'),
+                'skor_qty' => $this->input->post('skor_quality'),
+                'skor_abc' => $this->input->post('skor_abc'),
+                'skor_time' => $this->input->post('skor_time'),
+                'user_id' => $this->input->post('user_id'),
+            ];
+            $this->db->insert('mitra_track_record', $data);
+            $this->session->set_flashdata('message', 'Data berhasil ditambahkan!');
+        } else {
+            $this->session->set_flashdata('error', 'Isian data track record mitra harus diisi dengan lengkap!');
+        }
+        redirect('database_mitra/mitra_data');
     }
 }
--- application/views/database_mitra/mitra-data.php
+++ application/views/database_mitra/mitra-data.php
@@ -9,31 +9,31 @@
         </div>
     </div>
 
+
     <!-- Notification -->
     <div class="row">
-        <div class="col">
-            <?php
-            if ($this->session->flashdata('error') != '') {
-                echo '<div class="alert alert-danger" role="alert">';
-                echo $this->session->flashdata('error');
-                echo '</div>';
-            }
-            ?>
-            <?php
-            if ($this->session->flashdata('message') != '') {
-                echo '<div class="alert alert-success" role="alert">';
-                echo $this->session->flashdata('message');
-                echo '</div>';
-            }
-            ?>
+        <div class="col-lg-6">
+            <?php if ($this->session->flashdata('error') != '') : ?>
+                <div class="alert alert-danger" role="alert">
+                    <?= $this->session->flashdata('error'); ?>
+                </div>
+            <?php endif; ?>
+
+            <?php if ($this->session->flashdata('message') != '') : ?>
+                <div class="alert alert-success" role="alert">
+                    <?= $this->session->flashdata('message'); ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
+
 
     <!-- Page Content -->
     <div class="row">
         <div class="col">
-            <div class="table-responsive">
-                <table class="table table-hover dataTables-list" id="dataPerPeg" width="100%" cellspacing="0">
+            <a href="" class="btn btn-primary btn-sm mb-4" data-toggle="modal" data-target="#newMitraRecordModal"><i class="fas fa-user-plus mr-2"></i> Tambahkan Data Track Record Mitra</a>
+            <div class=" table-responsive">
+                <table class="table table-hover table-sm dataTables" id="dataMitraRecord" width="100%" cellspacing="0">
                     <thead class="thead-dark">
                         <tr>
                             <th class="text-center">#</th>
@@ -67,8 +67,8 @@
                                 <td class="text-center"><?= $key['skor_time']; ?></td>
                                 <td class="text-center"><?= $key['uname']; ?></td>
                                 <td class="text-center">
-                                    <a href="" class="badge badge-primary">Edit</a>
-                                    <a href="" class="badge badge-danger">Hapus</a>
+                                    <a href="" class="badge badge-pill badge-primary">Edit</a>
+                                    <a href="" class="badge badge-pill badge-danger">Hapus</a>
                                 </td>
                             </tr>
                             <?php $i++; ?>
@@ -86,3 +86,89 @@
 
 </div>
 <!-- End of Main Content -->
+
+
+
+<!-- New Mitra Track Record Modal -->
+<div class="modal fade" id="newMitraRecordModal" tabindex="-1" role="dialog" aria-labelledby="newMitraRecordModalLabel" data-backdrop="static" data-keyboard="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header bg-primary">
+                <h5 class="modal-title h5 text-light" id="newMitraRecordModalLabel">Tambah Data Track Record Mitra</h5>
+                <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fas fa-times"></i></span>
+                </button>
+            </div>
+
+            <form action="<?= base_url('database_mitra/mitra_data/addRecord'); ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-row">
+                        <div class="form-group col-lg">Informasi Mitra dan Kegiatan Statistik</div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg">
+                            <select name="mitra_id" id="mitra_id" class="form-control">
+                                <option value="">Pilih mitra yang akan dinilai</option>
+                                <?php foreach ($mitra as $key) : ?>
+                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['name']); ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg-6">
+                            <select name="activity_id" id="activity_id" class="form-control">
+                                <option value="">Pilih kegiatan statistik</option>
+                                <?php foreach ($activity as $key) : ?>
+                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['activity']); ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="year" name="year" placeholder="Tahun" required />
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg">Penilaian</div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_geo" name="skor_geo" placeholder="Nilai penguasaan wilayah (1-100)" required />
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_it" name="skor_it" placeholder="Nilai pemahaman teknologi (1-100)" required />
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_probing" name="skor_probing" placeholder="Nilai kemampuan probing (1-100)" required />
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_quality" name="skor_quality" placeholder="Nilai kualitas isian (1-100)" required />
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_abc" name="skor_abc" placeholder="Nilai kerapihan tulisan (1-100)" required />
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <input type="text" class="form-control" id="skor_time" name="skor_time" placeholder="Nilai ketepatan waktu (1-100)" required />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <select name="user_id" id="user_id" class="form-control">
+                            <option value="">Pilih penilai</option>
+                            <?php foreach ($reviewer as $key) : ?>
+                                <option value="<?= $key['nip']; ?>"><?= str_replace('_', ' ', $key['uname']); ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
