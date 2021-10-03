7d4778d477990375ef6ac06ceb39bfd0c7c02b0f
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-10-03 09:43:59 +0800

    fix activity manajemen

--- application/controllers/manajemen/Activity_list.php
+++ application/controllers/manajemen/Activity_list.php
@@ -21,7 +21,56 @@ class Activity_list extends CI_Controller
         $this->load->view('partials/header', $data);
         $this->load->view('partials/sidebar', $data);
         $this->load->view('partials/topbar', $data);
-        $this->load->view('manajemen/activity', $data);
+        $this->load->view('manajemen/activity-list', $data);
         $this->load->view('partials/footer');
+    }
+
+    public function addActivity()
+    {
+        $config = [
+            [
+                'field' => 'activity',
+                'label' => 'Activity',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Kegiatan tidak boleh kosong!' //ini udah engga perlu, karena di tag input modalnya udah dikasi 'required'
+                ],
+            ],
+        ];
+        $this->form_validation->set_rules($config);
+
+        if ($this->form_validation->run()) {
+            $data = [
+                'activity' => $this->input->post('activity')
+            ];
+            $this->db->insert('activity', $data);
+
+            $this->session->set_flashdata('message', 'Kegiatan baru berhasil ditambahkan!');
+        } else {
+            $this->session->set_flashdata('error', 'Isian kegiatan harus diisi!');
+        }
+        redirect('manajemen/activity_list');
+    }
+
+    public function editActivity()
+    {
+        $data = [
+            'id' => $this->input->post('id')
+        ];
+        $query = $this->db->get_where('activity', $data)->row_array();
+        // $query = $this->db->get_where('activity', ['id' => $this->session->userdata('email')])->row_array();
+        // $a = $this->input->post('id');
+
+        // $q = $this->db->get_where('activity', $data)->row_array();
+        // var_dump($q);
+
+        echo json_encode($query);
+    }
+
+    public function deleteActivity($id)
+    {
+        if ($this->db->delete('activity', ['id' => $id])) {
+            redirect('manajemen/activity_list');
+        }
     }
 }
