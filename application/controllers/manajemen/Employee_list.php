d20e0994b2c84a883fe100611a58f3cc1c171997
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-10-04 11:37:18 +0800

    fix employee manajemen

--- application/controllers/manajemen/Employee_list.php
+++ application/controllers/manajemen/Employee_list.php
@@ -14,6 +14,9 @@ class Employee_list extends CI_Controller
         $data['title'] = 'Daftar Pegawai BPS Kabupaten Wakatobi';
         $data['subMenuName'] = 'Pegawai';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['district'] = $this->db->get('district')->result_array();
+        $data['position'] = $this->db->get('user_position')->result_array();
+        $data['role'] = $this->db->get('user_role')->result_array();
 
         $query = $this->db->join('user_role', 'user.role_id = user_role.id')
             ->join('user_position', 'user.position_id = user_position.id')
@@ -30,30 +33,32 @@ class Employee_list extends CI_Controller
 
     public function addEmployee()
     {
-        // $this->form_validation->set_rules('name', 'Name', 'required');
-        // $this->form_validation->set_rules('village_id', 'Village', 'required');
-        // $this->form_validation->set_rules('birthdate', 'Birthdate', 'required');
-        // $this->form_validation->set_rules('gender', 'Gender', 'required');
-        // $this->form_validation->set_rules('phone', 'Phone', 'required');
-        // $this->form_validation->set_rules('marriage', 'Marriage', 'required');
-        // $this->form_validation->set_rules('education', 'Education', 'required');
-        // $this->form_validation->set_rules('job', 'Job', 'required');
+        $this->form_validation->set_rules('nip', 'Nip', 'required|min_length[9]|max_length[9]|numeric');
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('district_id', 'District', 'required');
+        $this->form_validation->set_rules('position_id', 'Position', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]');
+        $this->form_validation->set_rules('phone', 'Phone', 'required|numeric');
+        $this->form_validation->set_rules('gender', 'Gender', 'required');
+        $this->form_validation->set_rules('role_id', 'Role', 'required');
 
         if ($this->form_validation->run()) {
             $data = [
-                // 'name' => $this->input->post('name'),
-                // 'village_id' => $this->input->post('village_id'),
-                // 'date_of_birth' => $this->input->post('birthdate'),
-                // 'gender' => $this->input->post('gender'),
-                // 'phone' => $this->input->post('phone'),
-                // 'marriage_status' => $this->input->post('marriage'),
-                // 'education' => $this->input->post('education'),
-                // 'job' => $this->input->post('job'),
+                'nip' => $this->input->post('nip'),
+                'uname' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'password' => password_hash(123456, PASSWORD_DEFAULT),
+                'role_id' => $this->input->post('role_id'),
+                'gender' => $this->input->post('gender'),
+                'position_id' => $this->input->post('position_id'),
+                'district_id' => $this->input->post('district_id'),
+                'phone' => $this->input->post('phone'),
+                'image' => 'default.jpg',
             ];
             $this->db->insert('user', $data);
             $this->session->set_flashdata('message', 'Data berhasil ditambahkan.');
         } else {
-            $this->session->set_flashdata('error', 'Data pegawai harus diisi dengan lengkap!');
+            $this->session->set_flashdata('error', 'Data pegawai harus diisi dengan lengkap dan benar!');
         }
         redirect('manajemen/employee_list');
     }
--- application/views/manajemen/employee-list.php
+++ application/views/manajemen/employee-list.php
@@ -31,7 +31,7 @@
     <!-- Page Content -->
     <div class="row">
         <div class="col-lg">
-            <a href="" class="btn btn-primary btn-sm mb-4" data-toggle="modal" data-target="#newMitraModal"><i class="fas fa-user-plus mr-2"></i> Tambahkan Data Pegawai Baru</a>
+            <a href="" class="btn btn-primary btn-sm mb-4" data-toggle="modal" data-target="#newEmployeeModal"><i class="fas fa-user-plus mr-2"></i> Tambahkan Data Pegawai Baru</a>
             <div class=" table-responsive">
                 <table class="table table-hover table-sm dataTables" id="dataMitra" width="100%" cellspacing="0">
                     <thead class="thead-dark">
@@ -39,12 +39,12 @@
                             <th class="text-center">#</th>
                             <th class="text-center">NIP</th>
                             <th class="text-center">Nama</th>
+                            <th class="text-center">Wilayah</th>
+                            <th class="text-center">Jabatan</th>
                             <th class="text-center">Email</th>
                             <th class="text-center">Nomor HP</th>
-                            <th class="text-center">Role</th>
                             <th class="text-center">Jenis Kelamin</th>
-                            <th class="text-center">Jabatan</th>
-                            <th class="text-center">Wilayah</th>
+                            <th class="text-center">Role</th>
                             <th class="text-center">Action</th>
                         </tr>
                     </thead>
@@ -55,12 +55,12 @@
                                 <td class="text-center"><?= $i ?></td>
                                 <td class="text-center"><?= $key['nip']; ?></td>
                                 <td><?= $key['uname']; ?></td>
+                                <td class="text-center"><?= $key['district']; ?></td>
+                                <td class="text-center"><?= $key['position']; ?></td>
                                 <td class="text-center"><?= $key['email']; ?></td>
                                 <td class="text-center"><?= $key['phone']; ?></td>
-                                <td class="text-center"><?= $key['role']; ?></td>
                                 <td class="text-center"><?= $key['gender']; ?></td>
-                                <td class="text-center"><?= $key['position']; ?></td>
-                                <td class="text-center"><?= $key['district']; ?></td>
+                                <td class="text-center"><?= $key['role']; ?></td>
                                 <td class="text-center">
                                     <a href="" class="badge badge-pill badge-primary">Edit</a>
                                     <a href="" class="badge badge-pill badge-danger">Hapus</a>
@@ -85,37 +85,58 @@
 
 
 <!-- New Employee Data Modal -->
-<div class="modal fade" id="newMitraModal" tabindex="-1" role="dialog" aria-labelledby="newMitraModalLabel" data-backdrop="static" data-keyboard="true">
+<div class="modal fade" id="newEmployeeModal" tabindex="-1" role="dialog" aria-labelledby="newEmployeeModalLabel" data-backdrop="static" data-keyboard="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary">
-                <h5 class="modal-title h5 text-light" id="newMitraModalLabel">Tambah Data Mitra</h5>
+                <h5 class="modal-title h5 text-light" id="newEmployeeModalLabel">Menambahkan Data Pegawai Baru</h5>
                 <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true"><i class="fas fa-times"></i></span>
                 </button>
             </div>
-            <form action="<?= base_url('manajemen/mitra_list/addMitra'); ?>" method="post">
+            <form action="<?= base_url('manajemen/employee_list/addEmployee'); ?>" method="post">
                 <div class="modal-body">
                     <div class="form-row">
                         <div class="form-group col-lg-6">
-                            <label class="mt-2" for="name">Nama Mitra</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Tuliskan nama lengkap mitra" required />
+                            <label class="mt-2" for="nip">NIP Pegawai</label>
+                            <input type="text" class="form-control" id="nip" name="nip" placeholder="Tulisan NIP lama (9 digit)" required />
                         </div>
                         <div class="form-group col-lg-6">
-                            <label class="mt-2" for="village_id">Desa Asal</label>
-                            <select name="village_id" id="village_id" class="form-control">
-                                <option value="">--pilih desa asal mitra--</option>
-                                <?php foreach ($village as $key) : ?>
-                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['village']); ?></option>
+                            <label class="mt-2" for="name">Nama Pegawai</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Tuliskan nama lengkap pegawai" required />
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-lg-6">
+                            <label class="mt-2" for="district_id">Wilayah</label>
+                            <select name="district_id" id="district_id" class="form-control">
+                                <option value="">--pilih kecamatan--</option>
+                                <?php foreach ($district as $key) : ?>
+                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['district']); ?></option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-lg-6">
+                            <label class="mt-2" for="position_id">Jabatan</label>
+                            <select name="position_id" id="position_id" class="form-control">
+                                <option value="">--pilih jabatan--</option>
+                                <?php foreach ($position as $key) : ?>
+                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['position']); ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-lg-6">
-                            <label class="mt-2" for="birthdate">Tanggal Lahir</label>
-                            <input type="date" class="form-control" id="birthdate" name="birthdate" required />
+                            <label class="mt-2" for="email">Email</label>
+                            <input type="text" class="form-control" id="email" name="email" placeholder="" required />
                         </div>
+                        <div class="form-group col-lg-6">
+                            <label class="mt-2" for="phone">Nomor HP</label>
+                            <input type="text" class="form-control" id="phone" name="phone" placeholder="" required />
+                        </div>
+                    </div>
+                    <div class="form-row">
                         <div class="form-group col-lg-6">
                             <label class="mt-2" for="gender">Jenis Kelamin</label>
                             <select name="gender" id="gender" class="form-control">
@@ -124,39 +145,14 @@
                                 <option value="Laki-laki">Laki-laki</option>
                             </select>
                         </div>
-                    </div>
-                    <div class="form-row">
                         <div class="form-group col-lg-6">
-                            <label class="mt-2" for="phone">Nomor HP</label>
-                            <input type="text" class="form-control" id="phone" name="phone" placeholder="" required />
-                        </div>
-                        <div class="form-group col-lg-6">
-                            <label class="mt-2" for="marriage">Status Pernikahan</label>
-                            <select name="marriage" id="marriage" class="form-control">
-                                <option value="">--pilih status pernikahan--</option>
-                                <option value="Belum Menikah">Belum Menikah</option>
-                                <option value="Sudah Menikah">Sudah Menikah</option>
+                            <label class="mt-2" for="role_id">Role</label>
+                            <select name="role_id" id="role_id" class="form-control">
+                                <option value="">--pilih role--</option>
+                                <?php foreach ($role as $key) : ?>
+                                    <option value="<?= $key['id']; ?>"><?= str_replace('_', ' ', $key['role']); ?></option>
+                                <?php endforeach; ?>
                             </select>
-                        </div>
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-lg-6">
-                            <label class="mt-2" for="marriage">Pendidikan Terakhir yang Ditamatkan</label>
-                            <select name="education" id="education" class="form-control">
-                                <option value="">--pilih pendidikan terakhir--</option>
-                                <option value="SD/MI">SD/MI</option>
-                                <option value="SMP/MTs">SMP/MTs</option>
-                                <option value="SMA/MA/SMK">SMA/MA/SMK</option>
-                                <option value="DI">DI</option>
-                                <option value="DII">DII</option>
-                                <option value="DIII">DIII</option>
-                                <option value="DIV">DIV</option>
-                                <option value="S1">S1</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-lg-6">
-                            <label class="mt-2" for="job">Pekerjaan</label>
-                            <input type="text" class="form-control" id="job" name="job" placeholder="" required />
                         </div>
                     </div>
                 </div>
