0ca6af6e77938b1ab8bcdcca86686b24b9f75c12
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-10-11 22:16:52 +0800

    add new script

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -20,16 +20,16 @@
 </a>
 
 <!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="logoutModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Apakah yakin ingin keluar?</h5>
+                <h5 class="modal-title" id="logoutModalLabel">Apakah yakin ingin keluar?</h5>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
-            <div class="modal-body">Pilih "Logout" jika ingin mengakhiri sesi ini.</div>
+            <div class="modal-body">Pilih <B>Logout</B> jika ingin mengakhiri sesi ini.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
                 <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
@@ -47,6 +47,21 @@
 
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+
+
+<!-- NEW SCRIPT -->
+
+<script src="<?= base_url('assets/'); ?>js/script.js"></script>
+
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+<!-- Sweet Alert -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<!-- --------- -->
 
 </body>
 
