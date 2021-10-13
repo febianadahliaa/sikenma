edc8928e48edac579a876fa026a24b9f66a58c58
Febiana D. Anjani <febianadahliaa@FEBIANA-2.local>
2021-10-14 07:13:29 +0800

    fix config of base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sikenma/';
+// $config['base_url'] = 'http://localhost/sikenma/';
+$config['base_url'] = 'http://' . $_SERVER['HTTP_HOST'] . '/sikenma';
 
 /*
 |--------------------------------------------------------------------------
